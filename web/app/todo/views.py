91660d08b6a76781db440c0d08b1bdedbec8f5b8
Yoel Monzon <yoelfme@hotmail.com>
2015-08-19 23:22:26 -0600

    [add] view for home and routes

--- web/app/todo/views.py
+++ web/app/todo/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import Views
+from .models import Task
+from redis import Redis
 
-# Create your views here.
+redis = Redis(host='redis', port=6379)
+
+
+class HomeView(View):
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        counter = redis.incr('counter')
+        return render(request, 'my_home', {
+            'tasks': tasks,
+            'counter': counter
+        })
+
+    def post(self, request):
+        Task.objects.create(text=request.POST['text'])
+
+        return redirect(reverse('home'))
