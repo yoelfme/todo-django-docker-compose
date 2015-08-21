de2f9cfe725c5ca523fbb299d9922f317b7a5f9a
Yoel Monzon <yoelfme@hotmail.com>
2015-08-21 11:52:40 -0600

    [add] base templates and fix url

--- web/app/todo/views.py
+++ web/app/todo/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, redirect, reverse
-from django.views.generic import Views
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.views.generic import View
 from .models import Task
-from redis import Redis
+# from redis import Redis
 
-redis = Redis(host='redis', port=6379)
+# redis = Redis(host='redis', port=6379)
 
 
 class HomeView(View):
 
     def get(self, request):
         tasks = Task.objects.all()
-        counter = redis.incr('counter')
-        return render(request, 'my_home', {
+        # counter = redis.incr('counter')
+        counter = 0
+        return render(request, 'todo/_base.html', {
             'tasks': tasks,
             'counter': counter
         })
